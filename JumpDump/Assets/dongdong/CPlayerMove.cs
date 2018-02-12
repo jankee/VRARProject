880082716693d13233b0c2a235de4b2e78b67b79
user-PC\user <timeline_ing@naver.com>
2018-02-12 15:34:47 +0900

    테스트

---
+++ JumpDump/Assets/dongdong/CPlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPlayerMove : MonoBehaviour {
+
+    public float _moveSpeed;
+    //public float _gravity;
+    public float _rotSpeed;
+    //private Rigidbody _rigidbody;
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * v * _moveSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up * h * _rotSpeed * Time.deltaTime);
+
+        //_rigidbody.velocity = transform.forward * v * _moveSpeed;
+        //_rigidbody.angularVelocity = new Vector3(0f, h * _rotSpeed / 360f * Mathf.PI * 2f, 0f);
+    }
+}
