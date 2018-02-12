250e5f73ed3021b6ae72f59661ab02e0bbe82cf6
user-PC\user <jjankee@gmail.com>
2018-02-12 12:24:36 +0900

    인풋매니져 작업 중

--- JumpDump/Assets/Resource/Scripts/InputManager.cs
+++ JumpDump/Assets/Resource/Scripts/InputManager.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManager : MonoBehaviour {
+public class InputManager : MonoBehaviour
+{
+    //플레이어 담을 변수
+    private Player player;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    private void Start()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindObjectOfType<Player>();
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    //실제 인풋이 들어 오는 함수
+    public void HandleInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //마우스가 누른다
+            print("마우스가 누른다");
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            //마우스 드레그 중인가
+            print("마우스 드레그 중인가");
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            //마우스를 띄였을 때
+            print("마우스를 띄였을 때");
+        }
+    }
 }
