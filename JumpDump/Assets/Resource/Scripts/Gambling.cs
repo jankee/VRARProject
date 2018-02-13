df7184304d0ba7ce23fb2a46fce24242d0dcbb0d
user-PC\user <bellomoon92@gmail.com>
2018-02-13 11:44:40 +0900

    갬블링 추가

--- JumpDump/Assets/Resource/Scripts/Gambling.cs
+++ JumpDump/Assets/Resource/Scripts/Gambling.cs
@@ -2,33 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Gambling : MonoBehaviour {
 
     public Text _coinText;
-    public Text _resultCoin;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-
-	public void GambleButtonClick()
+    public Text _resultCoinText;
+    	       
+    // 뽑기 버튼 실행
+    public void GambleButtonClick()
     {
-        CoinGambleDown();
-       int coin = Random.Range(1, 500);
-        _resultCoin.text = coin.ToString();
-        _coinText.text = coin.ToString();
+        int value = int.Parse(_coinText.text);
+        int ranCoin = Random.Range(1, 500);
+        value -= 100;
+        _resultCoinText.text = ranCoin.ToString();
+        value = value + ranCoin;
+        _coinText.text = value.ToString();
     }
 
-
-    public void CoinGambleDown()
+   
+    // 인터페이스로 돌아감
+    /*public void ReturnBotton()
     {
-        int score = int.Parse(_coinText.text);
-
-        score -= 100;
-
-        _coinText.text = score.ToString();
-    }
+        SceneManager.LoadScene("");
+    }*/
 }
