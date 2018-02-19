bb09edecda6fe735f6a2ce2891e652c6a4a9a25f
user-PC\user <timeline_ing@naver.com>
2018-02-19 10:58:49 +0900

    카메라 수정중

--- JumpDump/Assets/Resource/Scripts/Camerafollow.cs
+++ JumpDump/Assets/Resource/Scripts/Camerafollow.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class Camerafollow : MonoBehaviour
 {
+    // 플레이어가 멈춰있을 때 카메라자동이동 속도
+    public float speed = 0.5f;
+
+    // 게임 종료 여부
+    public static bool IsGameStop = false;
+
     public GameObject playerMesh;
 
-    private Vector3 shouldPos;
+    private Vector3 shouldPos; // 플레이어 이동에 맞게 카메라 함께 이동
 
     private GameObject Coin;
 
     private void Update()
     {
-        //shouldPos = Vector3.Lerp(this.transform.position, playerMesh.transform.position, Time.deltaTime);
-        //this.transform.position = new Vector3(shouldPos.x, 4, 1
-        //    );
+        shouldPos = Vector3.Lerp(this.transform.position, playerMesh.transform.position, Time.deltaTime);
+        this.transform.position = new Vector3(shouldPos.x, 4, 1);
 
-        // + 지정된 시간에 카마레가 조금씩 위로 올라간다 > 카메라 설정 범위를 넘으면 죽는다.
+
+        // 게임이 시작되면
+        if (IsGameStop) 
+        {
+            //카메라가 자동으로 앞으로 이동함
+            transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed);
+        }
+
     }
+
+
 }
