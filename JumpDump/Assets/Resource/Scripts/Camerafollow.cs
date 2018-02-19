2339f2529d229860624ad511f549ea3057d8db4e
user-PC\user <timeline_ing@naver.com>
2018-02-19 11:50:56 +0900

    카메라 자동이동기능 수정중

--- JumpDump/Assets/Resource/Scripts/Camerafollow.cs
+++ JumpDump/Assets/Resource/Scripts/Camerafollow.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Camerafollow : MonoBehaviour
 {
+    // 카메라 지정
+    Camera cam;
+
     // 플레이어가 멈춰있을 때 카메라자동이동 속도
-    public float speed = 0.5f;
+    public float speed = 0.05f;
 
     // 게임 종료 여부
     public static bool IsGameStop = false;
@@ -18,16 +21,34 @@ public class Camerafollow : MonoBehaviour
 
     private void Update()
     {
-        shouldPos = Vector3.Lerp(this.transform.position, playerMesh.transform.position, Time.deltaTime);
+        shouldPos = Vector3.Lerp(this.transform.position, GameManager.Instance.Player.transform.position, Time.deltaTime);
         this.transform.position = new Vector3(shouldPos.x, 4, 1);
-
-
+      
         // 게임이 시작되면
         if (IsGameStop) 
         {
-            //카메라가 자동으로 앞으로 이동함
+            //카메라가 자동으로 앞으로 서서히 이동함
             transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed);
         }
+
+
+        // 카메라 범위 밖으로 플레이어가 닿으면 
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+            // 게임 종료
+            if (IsGameStop)
+            {
+                // 이동을 정지
+                Time.timeScale = 0;
+            }
+
+            // 게임 지속 
+            else
+            print("게임 지속");
+
 
     }
 
