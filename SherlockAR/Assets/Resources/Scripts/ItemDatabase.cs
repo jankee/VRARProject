0796825e1e7c7706f71a1beb0060bc29bfe9dd49
JANKEE-OFFICE\jankee <jjankee@gmail.com>
2018-04-26 16:00:07 +0900

    https://youtu.be/04_qOAHGtpk?list=PLivfKP2ufIK6ToVMtpc_KTHlJRZjuE1z0&t=43

--- SherlockAR/Assets/Resources/Scripts/ItemDatabase.cs
+++ SherlockAR/Assets/Resources/Scripts/ItemDatabase.cs
@@ -33,9 +33,23 @@ public class ItemDatabase : MonoBehaviour
         {
             for (int i = 0; i < item.Stats.Count; i++)
             {
+                print(item.ItemName);
                 print(item.Stats[i].StatName);
                 print(item.Stats[i].GetCalculatedStatValue());
             }
         }
+    }
+
+    public Item GetItem(string itemSlug)
+    {
+        foreach (Item item in Items)
+        {
+            if (item.ObjectSlug == itemSlug)
+            {
+                return item;
+            }
+        }
+        print("Couldn't find item : " + itemSlug);
+        return null;
     }
 }
